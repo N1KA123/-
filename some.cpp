56758f7370077bc89f1a36f11a6ef566a884b8e9
N1KA123 <nikabuzaladze06@gmail.com>
2024-04-18 16:55:49 +0400

    added logging levels

--- some.cpp
+++ some.cpp
@@ -4,38 +4,63 @@
 
 using namespace std;
 
+enum class LogLevel
+{
+    INFO,
+    WARNING,
+    ERROR
+};
+
 class Logger
 {
 public:
-    static void Log(const string &massage)
+    static void Log(const string &message, LogLevel level = LogLevel::INFO)
     {
-        validate(massage);
-        cout << massage << endl;
+        validate(message);
+        cout << "[" << levelToString(level) << "] " << message << endl;
     }
-    static void LogToFile(const string &massage)
+
+    static void LogToFile(const string &message, LogLevel level = LogLevel::INFO)
     {
-        ofstream MyFile("filename.txt");
-        MyFile<<massage;
+        ofstream MyFile("filename.txt", ios_base::app);
+        MyFile << "[" << levelToString(level) << "] " << message << endl;
     }
 
 private:
-    static void validate(const string &massage)
+    static void validate(const string &message)
     {
-        if (massage.length() == 0)
-            throw invalid_argument("massage is empty.");
+        if (message.empty())
+            throw invalid_argument("Message is empty.");
 
-        for (char i : massage)
+        for (char i : message)
         {
-            if ((i < 'a' && i > 'z') && i != ' ')
-                throw invalid_argument("massage contains invalid symbols.");
+            if (!((i >= 'a' && i <= 'z') || i == ' '))
+                throw invalid_argument("Message contains invalid symbols.");
+        }
+    }
+
+    static string levelToString(LogLevel level)
+    {
+        switch (level)
+        {
+        case LogLevel::INFO:
+            return "INFO";
+        case LogLevel::WARNING:
+            return "WARNING";
+        case LogLevel::ERROR:
+            return "ERROR";
+        default:
+            return "UNKNOWN";
         }
     }
 };
 
-
-
 int main()
 {
-    Logger::Log("something is good thing");
-    Logger::LogToFile("something is good thing");
+    Logger::Log("Something good happened.", LogLevel::INFO);
+    Logger::LogToFile("Something good happened.", LogLevel::INFO);
+    Logger::Log("Something suspicious happened.", LogLevel::WARNING);
+    Logger::LogToFile("Something suspicious happened.", LogLevel::WARNING);
+    Logger::Log("An error occurred.", LogLevel::ERROR);
+    Logger::LogToFile("An error occurred.", LogLevel::ERROR);
 }
